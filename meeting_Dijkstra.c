25649362d71103707f8952c11068378b1bc9e3ea
godimode <kottttt@naver.com>
2021-04-13 01:50:30 +0900

    modify code

--- meeting_Dijkstra.c
+++ meeting_Dijkstra.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#define Swap(a,b,type) do{\
-    type temp;\
-    temp=a;\
-    a=b;\
-    b=temp;\
-}while(0)
 
-#define MAX_NODE 50001
-#define MAX_EDGE 500001
+#define MAX_NODE 1000001
+#define MAX_EDGE 2000001
 #define INF 2100000000
 typedef struct {
     int node;
@@ -31,51 +25,6 @@ void init() {
     heap_len = 0;
     for (int i = 0; i <= m; i++)
         last_edge[i] = last_edge2[i] = 0;
-}
-int get_min_idx(int idx1, int idx2) {
-    return heap[idx1].cnt > heap[idx2].cnt ? idx2 : idx1;
-}
-
-void heap_push(Heap new_item)
-{
-    if (heap_len == 0) {
-        heap[++heap_len] = new_item;
-    }
-    else if (heap_len > 0) {
-        heap[++heap_len] = new_item;
-        int x = heap_len;
-        while (x > 1) {
-            if (heap[x / 2].cnt > heap[x].cnt) {
-                Swap(heap[x], heap[x / 2], Heap);
-                x /= 2;
-            }
-            else break;
-        }
-    }
-}
-
-Heap heap_pop()
-{
-    Heap min_heap = heap[1];
-    heap[1] = heap[heap_len--];
-    int x = 1;
-    while (x <= heap_len / 2) {
-        if (2 * x + 1 > heap_len) {
-            if (heap[2 * x].cnt < heap[x].cnt) {
-                Swap(heap[x], heap[2 * x], Heap);
-            }
-            break;
-        }
-        else {
-            int min_idx = get_min_idx(2 * x, 2 * x + 1);
-            if (heap[x].cnt > heap[min_idx].cnt) {
-                Swap(heap[x], heap[min_idx], Heap);
-                x = min_idx;
-            }
-            else break;
-        }
-    }
-    return min_heap;
 }
 
 void heap_Push(Heap item) {
@@ -126,7 +75,7 @@ Heap heap_Pop() {
 }
 int main()
 {
-    freopen("eval_input.txt", "r", stdin);
+   // freopen("eval_input.txt", "r", stdin);
     setbuf(stdout, NULL);
     int TC;
     scanf("%d", &TC);
@@ -145,38 +94,37 @@ int main()
             edge2[i].prev = last_edge2[edge2[i].start];
             last_edge2[edge2[i].start] = i;
         }
-        //if(t<=17) continue;
         for (int i = 0; i <= n; i++)
             dist[i] = dist2[i] = INF;
         Heap newHeap;
         newHeap.node = start, newHeap.cnt = 0;
-        heap_push(newHeap);
+        heap_Push(newHeap);
         dist[start] = 0;
         while (heap_len > 0) {
-            Heap next = heap_pop();
+            Heap next = heap_Pop();
             if (next.cnt > dist[next.node]) continue;
             for (int i = last_edge[next.node]; i; i = edge[i].prev) {
                 Edge e = edge[i];
                 if (dist[e.end] > dist[next.node] + e.cnt) {
                     dist[e.end] = dist[next.node] + e.cnt;
                     newHeap.node = e.end, newHeap.cnt = dist[e.end];
-                    heap_push(newHeap);
+                    heap_Push(newHeap);
                 }
             }
         }
         heap_len = 0;
         newHeap.node = start, newHeap.cnt = 0;
-        heap_push(newHeap);
+        heap_Push(newHeap);
         dist2[start] = 0;
         while (heap_len > 0) {
-            Heap next = heap_pop();
+            Heap next = heap_Pop();
             if (next.cnt > dist2[next.node]) continue;
             for (int i = last_edge2[next.node]; i; i = edge2[i].prev) {
                 Edge e = edge2[i];
                 if (dist2[e.end] > dist2[next.node] + e.cnt) {
                     dist2[e.end] = dist2[next.node] + e.cnt;
                     newHeap.node = e.end, newHeap.cnt = dist2[e.end];
-                    heap_push(newHeap);
+                    heap_Push(newHeap);
                 }
             }
         }
